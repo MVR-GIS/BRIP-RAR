f8a48467318c75f8b7dcf85bf58eca8729c58114
Michael Dougherty <mikedoc.p.doc.71@gmail.com>
2021-10-20 19:41:25 -0500

    updated risk_register_table function to handle zero records

--- R/risk_register_table.R
+++ R/risk_register_table.R
@@ -7,20 +7,25 @@
 #' @return `knitr::kable` table of risk items.
 #'
 risk_register_table <- function(risk) {
+  # Select for the needed fields
   risk_df <- risk %>%
     select(risk_no_link, CONCERNS, RISKCATEGORY, RISK_STRATEGY,
            COST_RISK, cost_risk_color,
            SCHEDULE_RISK, schedule_risk_color)
 
+  # Create the unstyled kable
+  risk_register <- kbl(select(risk_df,
+                              risk_no_link, CONCERNS, RISK_STRATEGY,
+                              COST_RISK, SCHEDULE_RISK),
+                       col.names = c("Number", "Description", "Strategy",
+                                     "Cost", "Schedule")) %>%
+    kable_styling(bootstrap_options = c("striped", "hover",
+                                        "condensed", "responsive"),
+                  font_size = 12)
 
-  risk_register <- kbl(
-    select(risk_df,
-             risk_no_link, CONCERNS, RISK_STRATEGY, COST_RISK, SCHEDULE_RISK),
-      col.names = c("Number", "Description", "Strategy", "Cost",
-                    "Schedule")) %>%
-    kable_styling(bootstrap_options = c("striped", "hover", "condensed",
-                                        "responsive"),
-                  font_size = 12) %>%
+  # Group rows if records exist
+  if(length(risk$RISK_NO) > 1) {
+    risk_register <- risk_register %>%
     pack_rows(index = table(risk_df$RISKCATEGORY),
               indent = FALSE,
               label_row_css = "background-color: #dad8d8; color: #000000;") %>%
@@ -28,6 +33,7 @@ risk_register_table <- function(risk) {
                 background = risk_df$cost_risk_color) %>%
     column_spec(5, color = "black",
                 background = risk_df$schedule_risk_color)
+  }
 
   return(risk_register)
 }
